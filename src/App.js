689519628e4b59813727b5df7735c61c84a96470
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-04 16:33:51 -0400

    first commit for api restaurants

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [allRestaurants, setAllRestaurants] = useState()
+  const [text, setText] = useState('text should render here')
+
+  useEffect(() => {
+    fetch('https://bocacode-intranet-api.web.app/restaurants')
+      .then(response => response.json())
+      .then(promise => setAllRestaurants(promise))
+      .catch(err => console.log(err))
+  }, [])
+
+  function handleSubmit(event) {
+    console.log(allRestaurants)
+    console.log(event)
+    event.preventDefault();
+
+    const newRestaurant = {
+      address: "301 N University Dr, Pembroke Pines, FL 33024",
+    }
+
+    fetch('https://bocacode-intranet-api.web.app/restaurants')
+
+    console.log('here is our text 2222')
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        <label htmlFor="">
+          <input
+            type="text"
+            onChange={event => setText(event.target.value)}
+            value={text} />
+        </label>
+        <button onClick={event => handleSubmit(event)}>go here</button>
+      </form>
+      <span>{text}</span>
+
+      {!allRestaurants ? <p>Loading...</p> : allRestaurants.map(restaurant => {
+        return (
+          <div className="App" key={restaurant.id}>
+            <h2>{restaurant.name}</h2>
+            <span>{restaurant.address}</span>
+            <span>{restaurant.cuisine}</span>
+            <span>{restaurant.rating}</span>
+            <img
+              alt={`The awesome ${restaurant.name}`}
+              src={restaurant.photoUrl}
+              style={{ maxWidth: '300px', padding: '12px' }}
+            />
+          </div>
+        )
+      })}
+
     </div>
   );
 }
